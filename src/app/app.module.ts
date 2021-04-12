ee9eecf4cd960ea4cf69153ebc7d25e58ec88077
Aysha-Akter <ayshakt3r@gmail.com>
2021-04-12 11:51:39 +0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AyshaModule } from './Aysha/Aysha.module';
+import { AyshaModule } from "./Aysha/Aysha.module";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AccordionModule } from "ngx-bootstrap/accordion";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AyshaModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AyshaModule, AccordionModule.forRoot()],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
