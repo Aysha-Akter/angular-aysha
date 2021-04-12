2fe4f49a6e8583aec4b9bebeb5e3d0c0271a0596
Aysha-Akter <ayshakt3r@gmail.com>
2021-04-12 13:02:14 +0600

    1 file modified

--- src/app/Aysha/Aysha.module.ts
+++ src/app/Aysha/Aysha.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from   '@angular/platform-browser/animations';
 import { DesignComponent } from './Design/Design.component';
 import { MyNavbarComponent } from './my-navbar/my-navbar.component';
 import { ListGroupComponent } from './list-group/list-group.component';
 import { MyBadgeComponent } from './my-badge/my-badge.component';
 import { AccordionComponent } from './accordion/accordion.component';
-import { AccordionModule } from "ngx-bootstrap";
-import { BrowserAnimationsModule } from   '@angular/platform-browser/animations';
+import { AccordionModule,  } from 'ngx-bootstrap/accordion';
+
+
 
 
 
