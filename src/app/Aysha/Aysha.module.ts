bc2d6f6babfb324a1bc3e16488df7d33fb80f02f
Aysha-Akter <ayshakt3r@gmail.com>
2021-04-13 12:52:40 +0600

    1 file modified

--- src/app/Aysha/Aysha.module.ts
+++ src/app/Aysha/Aysha.module.ts
@@ -63,6 +63,7 @@ import { ScrollspyComponent } from './scrollspy/scrollspy.component';
     AccordionCollapseComponent,
     PaginationComponent,
     ProgressComponent,
+    ScrollspyComponent
 
   ]
 })
