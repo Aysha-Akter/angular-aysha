41aa38611f578f556b88e9a6b719e0c115fbc444
Aysha-Akter <ayshakt3r@gmail.com>
2021-04-15 14:09:43 +0600

    1 file added

---
+++ src/app/new/new.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppModule } from "./app/app.module";
+
+@NgModule({
+  imports: [
+    CommonModule, AppModule
+  ],
+  declarations: []
+})
+export class NewModule { }
